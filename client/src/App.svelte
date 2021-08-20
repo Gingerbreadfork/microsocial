8168c68098291bc3e5cec3ecfbebd78e77b67ee5
Gingerbreadfork <gingerbreadfork@protonmail.com>
2021-08-21 06:34:30 +1000

    Silly Text Overflow Fix

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -40,12 +40,12 @@
     }
 </script>
 
-<div class="border-b border-gray-900 bg-black py-2 px-2 cursor-default">
+<div class="px-2 py-2 bg-black border-b border-gray-900 cursor-default">
     <div class="container mx-auto">
-        <nav class="flex items-center justify-between flex-wrap">
-            <div class="flex items-center flex-no-shrink text-white mr-6">
+        <nav class="flex flex-wrap items-center justify-between">
+            <div class="flex items-center mr-6 text-white flex-no-shrink">
                 <svg
-                    class="h-8 w-8 mr-2 text-purple-500"
+                    class="w-8 h-8 mr-2 text-purple-500"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -55,16 +55,16 @@
                         clip-rule="evenodd"
                     /></svg
                 >
-                <span class="font-semibold text-xl tracking-tight"
+                <span class="text-xl font-semibold tracking-tight"
                     >Microsocial</span
                 >
             </div>
             <div class="block sm:hidden">
                 <button
-                    class="navbar-burger flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white focus:outline-none"
+                    class="flex items-center px-3 py-2 text-white border border-white rounded navbar-burger hover:text-white hover:border-white focus:outline-none"
                 >
                     <svg
-                        class="fill-current h-3 w-3"
+                        class="w-3 h-3 fill-current"
                         viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg"
                         ><title>Microsocial</title><path
@@ -75,9 +75,9 @@
             </div>
             <div
                 id="main-nav"
-                class="w-full gap-4 flex-grow sm:flex items-center sm:w-auto hidden"
+                class="items-center flex-grow hidden w-full gap-4 sm:flex sm:w-auto"
             >
-                <div class="text-sm sm:flex-grow text-white" />
+                <div class="text-sm text-white sm:flex-grow" />
                 {#if currentComponent != "Feed"}
                     <button
                         title="Live Feed"
@@ -85,7 +85,7 @@
                             toggleBurger();
                             currentComponent = "Feed";
                         }}
-                        class="font-medium text-gray-200 hover:text-white flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-gray-200 hover:text-white sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -94,7 +94,7 @@
                             ><path
                                 d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Live Feed</span
+                        ><span class="ml-2 text-white sm:hidden">Live Feed</span
                         ></button
                     >
                 {:else}
@@ -104,7 +104,7 @@
                             toggleBurger();
                             currentComponent = "Feed";
                         }}
-                        class="font-medium text-indigo-500 hover:text-indigo-400 flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-indigo-500 hover:text-indigo-400 sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -113,7 +113,7 @@
                             ><path
                                 d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Live Feed</span
+                        ><span class="ml-2 text-white sm:hidden">Live Feed</span
                         ></button
                     >
                 {/if}
@@ -124,7 +124,7 @@
                             toggleBurger();
                             currentComponent = "Messages";
                         }}
-                        class="font-medium text-gray-200 hover:text-white flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-gray-200 hover:text-white sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -135,7 +135,7 @@
                                 d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
                                 clip-rule="evenodd"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Messages</span
+                        ><span class="ml-2 text-white sm:hidden">Messages</span
                         ></button
                     >
                 {:else}
@@ -145,7 +145,7 @@
                             toggleBurger();
                             currentComponent = "Messages";
                         }}
-                        class="font-medium text-indigo-500 hover:text-indigo-400 flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-indigo-500 hover:text-indigo-400 sm:pt-0 focus:outline-none"
                     >
                         <svg
                             class="w-6 h-6"
@@ -157,7 +157,7 @@
                                 d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
                                 clip-rule="evenodd"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Messages</span
+                        ><span class="ml-2 text-white sm:hidden">Messages</span
                         ></button
                     >
                 {/if}
@@ -168,7 +168,7 @@
                             toggleBurger();
                             currentComponent = "Profile";
                         }}
-                        class="font-medium text-gray-200 hover:text-white flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-gray-200 hover:text-white sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -179,7 +179,7 @@
                                 d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
                                 clip-rule="evenodd"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Profile</span
+                        ><span class="ml-2 text-white sm:hidden">Profile</span
                         ></button
                     >
                 {:else}
@@ -189,7 +189,7 @@
                             toggleBurger();
                             currentComponent = "Profile";
                         }}
-                        class="font-medium text-indigo-500 hover:text-indigo-400 flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-indigo-500 hover:text-indigo-400 sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -200,7 +200,7 @@
                                 d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
                                 clip-rule="evenodd"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Profile</span
+                        ><span class="ml-2 text-white sm:hidden">Profile</span
                         ></button
                     >
                 {/if}
@@ -211,7 +211,7 @@
                             toggleBurger();
                             currentComponent = "Friends";
                         }}
-                        class="font-medium text-gray-200 hover:text-white flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-gray-200 hover:text-white sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -220,7 +220,7 @@
                             ><path
                                 d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Friends</span
+                        ><span class="ml-2 text-white sm:hidden">Friends</span
                         ></button
                     >{:else}
                     <button
@@ -229,7 +229,7 @@
                             toggleBurger();
                             currentComponent = "Friends";
                         }}
-                        class="font-medium text-indigo-500 hover:text-indigo-400 flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-indigo-500 hover:text-indigo-400 sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -238,7 +238,7 @@
                             ><path
                                 d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
                             /></svg
-                        ><span class="text-white sm:hidden ml-2">Friends</span
+                        ><span class="ml-2 text-white sm:hidden">Friends</span
                         ></button
                     >
                 {/if}
@@ -249,7 +249,7 @@
                             toggleBurger();
                             currentComponent = "Settings";
                         }}
-                        class="font-medium text-gray-200 hover:text-white flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-gray-200 hover:text-white sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -260,7 +260,7 @@
                                 d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
                                 clip-rule="evenodd"
                             />
-                        </svg><span class="text-white sm:hidden ml-2"
+                        </svg><span class="ml-2 text-white sm:hidden"
                             >Settings</span
                         ></button
                     >
@@ -271,7 +271,7 @@
                             toggleBurger();
                             currentComponent = "Settings";
                         }}
-                        class="font-medium text-indigo-500 hover:text-indigo-400 flex pt-2 sm:pt-0 m-auto focus:outline-none"
+                        class="flex pt-2 m-auto font-medium text-indigo-500 hover:text-indigo-400 sm:pt-0 focus:outline-none"
                         ><svg
                             class="w-6 h-6"
                             fill="currentColor"
@@ -282,7 +282,7 @@
                                 d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
                                 clip-rule="evenodd"
                             />
-                        </svg><span class="text-white sm:hidden ml-2"
+                        </svg><span class="ml-2 text-white sm:hidden"
                             >Settings</span
                         ></button
                     >
@@ -292,7 +292,7 @@
     </div>
 </div>
 
-<div class="p-2 bg-gray-300 min-h-screen">
+<div class="min-h-screen p-2 break-words bg-gray-300">
     {#if currentComponent == "Friends"}
         <ManageFriends />
     {:else if currentComponent == "Settings"}
