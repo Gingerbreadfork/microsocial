b7828c8add15dd0a8cd0a89bf82a52b7639d3c31
Gingerbreadfork <gingerbreadfork@protonmail.com>
2021-08-31 23:04:41 +1000

    Remove Duplicate Explainer

--- client/src/components/ManageFriends.svelte
+++ client/src/components/ManageFriends.svelte
@@ -489,39 +489,4 @@
             {/each}
         {/if}
     {/if}
-
-    {#if friendListLoaded && actualFriendCount == 0 && pendingFriendCount == 0}
-        <div
-            class="flex items-center justify-center p-8 mt-10 bg-gray-200 shadow-md hover:shodow-lg rounded-2xl dark:bg-truegray-800 dark:border-truegray-900"
-        >
-            <div class="flex items-center">
-                <svg
-                    class="w-20 h-20 text-blue-400 animate-pulse"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                    ><path
-                        fill-rule="evenodd"
-                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                        clip-rule="evenodd"
-                    /></svg
-                >
-            </div>
-            <div class="flex flex-col ml-3">
-                <div class="font-medium leading-none">
-                    Want to add a Friend?
-                </div>
-                <p
-                    class="mt-1 text-sm leading-none text-gray-600 dark:text-truegray-300"
-                >
-                    Use the form above to send your friend a request using their
-                    Microsocial URL. If they accept your request you'll get one
-                    back just to confirm the connection request. For example,
-                    your server URL would be <b
-                        >https://{window.location.hostname}/
-                    </b>
-                </p>
-            </div>
-        </div>
-    {/if}
 </div>
