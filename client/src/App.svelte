84b62adee930bc79f3f888ce1b2333142157e2d0
Gingerbreadfork <gingerbreadfork@protonmail.com>
2021-09-07 09:37:14 +1000

    Add basic Landing Page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -8,7 +8,7 @@
     import { onMount } from "svelte";
 
     let darkMode;
-    let currentComponent = "Feed";
+    let currentComponent = "Landing";
 
     onMount(async () => {
         // Navbar Toggle
@@ -63,10 +63,15 @@
     }
 </script>
 
-<div class="px-2 py-2 border-b border-gray-900 cursor-default bg-truegray-900">
+<div class="px-2 py-2 cursor-default bg-truegray-900">
     <div class="container mx-auto">
         <nav class="flex flex-wrap items-center justify-between">
-            <div class="flex items-center mr-6 text-white flex-no-shrink">
+            <div
+                on:click={() => {
+                    currentComponent = "Landing";
+                }}
+                class="flex items-center mr-6 text-white cursor-pointer flex-no-shrink"
+            >
                 <svg
                     class="w-8 h-8 mr-2 text-purple-500"
                     fill="currentColor"
@@ -82,6 +87,7 @@
                     >Microsocial</span
                 >
             </div>
+
             <div class="block sm:hidden">
                 <button
                     class="flex items-center px-3 py-2 text-white border border-white rounded navbar-burger hover:text-white hover:border-white focus:outline-none"
@@ -337,7 +343,7 @@
                         on:click={toggleTheme}
                         class="p-2"
                         ><svg
-                            class="w-6 h-6 text-blue-200"
+                            class="w-6 h-6 text-indigo-200"
                             fill="none"
                             stroke="currentColor"
                             viewBox="0 0 24 24"
@@ -357,9 +363,167 @@
 </div>
 
 <div
-    class="min-h-screen p-2 break-words bg-gray-300 dark:bg-truegray-900 dark:text-white"
+    class="min-h-screen break-words bg-gray-300 dark:bg-truegray-900 dark:text-white"
 >
-    {#if currentComponent == "Friends"}
+    {#if currentComponent == "Landing"}
+        <div
+            class="min-h-screen overflow-auto bg-gradient-to-br from-indigo-900 to-purple-900 dark:from-truegray-900 dark:to-truegray-900"
+        >
+            <div class="container max-w-6xl px-6 py-20 mx-auto">
+                <div
+                    class="w-full pb-6 mx-auto text-center md:w-11/12 xl:w-9/12"
+                >
+                    <h1
+                        class="mb-2 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight"
+                    >
+                        <svg
+                            class="inline-block w-10 pb-2 text-purple-500 md:w-14 animate-bounce"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                            ><path
+                                fill-rule="evenodd"
+                                d="M9.504 1.132a1 1 0 01.992 0l1.75 1a1 1 0 11-.992 1.736L10 3.152l-1.254.716a1 1 0 11-.992-1.736l1.75-1zM5.618 4.504a1 1 0 01-.372 1.364L5.016 6l.23.132a1 1 0 11-.992 1.736L4 7.723V8a1 1 0 01-2 0V6a.996.996 0 01.52-.878l1.734-.99a1 1 0 011.364.372zm8.764 0a1 1 0 011.364-.372l1.733.99A1.002 1.002 0 0118 6v2a1 1 0 11-2 0v-.277l-.254.145a1 1 0 11-.992-1.736l.23-.132-.23-.132a1 1 0 01-.372-1.364zm-7 4a1 1 0 011.364-.372L10 8.848l1.254-.716a1 1 0 11.992 1.736L11 10.58V12a1 1 0 11-2 0v-1.42l-1.246-.712a1 1 0 01-.372-1.364zM3 11a1 1 0 011 1v1.42l1.246.712a1 1 0 11-.992 1.736l-1.75-1A1 1 0 012 14v-2a1 1 0 011-1zm14 0a1 1 0 011 1v2a1 1 0 01-.504.868l-1.75 1a1 1 0 11-.992-1.736L16 13.42V12a1 1 0 011-1zm-9.618 5.504a1 1 0 011.364-.372l.254.145V16a1 1 0 112 0v.277l.254-.145a1 1 0 11.992 1.736l-1.735.992a.995.995 0 01-1.022 0l-1.735-.992a1 1 0 01-.372-1.364z"
+                                clip-rule="evenodd"
+                            /></svg
+                        >
+                        <span
+                            class="inline w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-indigo-500 to-indigo-600"
+                        >
+                            Microsocial
+                        </span>
+                    </h1>
+                    <p
+                        class="px-0 mb-6 text-sm text-gray-300 md:text-xl lg:px-24"
+                    >
+                        Your Experimental Peer-to-Peer Social Platform
+                    </p>
+                </div>
+
+                <div class="relative text-gray-200">
+                    <div
+                        class="grid gap-1 pb-20 uppercase sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-6"
+                    >
+                        <div
+                            on:click={() => {
+                                currentComponent = "Feed";
+                            }}
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                        >
+                            <svg
+                                class="w-9"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                ><path
+                                    d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"
+                                /></svg
+                            >
+                            <div>
+                                <span>Live Feed</span>
+                                <span class="block text-xs text-indigo-300"
+                                    >Post with your friends</span
+                                >
+                            </div>
+                            <div>
+                                <i
+                                    class="block transition transform -translate-x-1 opacity-0 fa fa-chevron-right group-hover:opacity-100 group-hover:translate-x-0"
+                                />
+                            </div>
+                        </div>
+
+                        <div
+                            on:click={() => {
+                                currentComponent = "Messages";
+                            }}
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                        >
+                            <svg
+                                class="w-9"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                ><path
+                                    fill-rule="evenodd"
+                                    d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                                    clip-rule="evenodd"
+                                /></svg
+                            >
+                            <div>
+                                <span>Messaging</span>
+                                <span class="block text-xs text-indigo-300"
+                                    >Chat in Private</span
+                                >
+                            </div>
+                            <div>
+                                <i
+                                    class="block transition transform -translate-x-1 opacity-0 fa fa-chevron-right group-hover:opacity-100 group-hover:translate-x-0"
+                                />
+                            </div>
+                        </div>
+
+                        <div
+                            on:click={() => {
+                                currentComponent = "Profile";
+                            }}
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                        >
+                            <svg
+                                class="w-9"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                ><path
+                                    fill-rule="evenodd"
+                                    d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
+                                    clip-rule="evenodd"
+                                /></svg
+                            >
+                            <div>
+                                <span>Profile</span>
+                                <span class="block text-xs text-indigo-300"
+                                    >Your posts and bio</span
+                                >
+                            </div>
+                            <div>
+                                <i
+                                    class="block transition transform -translate-x-1 opacity-0 fa fa-chevron-right group-hover:opacity-100 group-hover:translate-x-0"
+                                />
+                            </div>
+                        </div>
+
+                        <div
+                            on:click={() => {
+                                currentComponent = "Friends";
+                            }}
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                        >
+                            <svg
+                                class="w-9"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                                ><path
+                                    d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
+                                /></svg
+                            >
+                            <div>
+                                <span>Manage Friends</span>
+                                <span class="block text-xs text-indigo-300"
+                                    >Add/Remove Friends</span
+                                >
+                            </div>
+                            <div>
+                                <i
+                                    class="block transition transform -translate-x-1 opacity-0 fa fa-chevron-right group-hover:opacity-100 group-hover:translate-x-0"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    {:else if currentComponent == "Friends"}
         <ManageFriends />
     {:else if currentComponent == "Settings"}
         <Settings />
