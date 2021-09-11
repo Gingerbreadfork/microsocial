788dd36c5fb66608cb965226e007f84092f17a52
Gingerbreadfork <gingerbreadfork@protonmail.com>
2021-09-11 15:31:06 +1000

    Landing Page Fleshed Out More

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -402,13 +402,13 @@
 
                 <div class="relative text-gray-200">
                     <div
-                        class="grid gap-1 pb-20 uppercase sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-6"
+                        class="grid gap-1 pb-10 uppercase sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-6"
                     >
                         <div
                             on:click={() => {
                                 currentComponent = "Feed";
                             }}
-                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-800 dark:hover:bg-indigo-900 hover:bg-opacity-100"
                         >
                             <svg
                                 class="w-9"
@@ -436,7 +436,7 @@
                             on:click={() => {
                                 currentComponent = "Messages";
                             }}
-                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-800 dark:hover:bg-indigo-900 hover:bg-opacity-100"
                         >
                             <svg
                                 class="w-9"
@@ -466,7 +466,7 @@
                             on:click={() => {
                                 currentComponent = "Profile";
                             }}
-                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-800 dark:hover:bg-indigo-900 hover:bg-opacity-100"
                         >
                             <svg
                                 class="w-9"
@@ -496,7 +496,7 @@
                             on:click={() => {
                                 currentComponent = "Friends";
                             }}
-                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-900 hover:bg-opacity-100"
+                            class="flex items-center gap-5 px-2 py-2 mt-5 transition bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-800 dark:hover:bg-indigo-900 hover:bg-opacity-100"
                         >
                             <svg
                                 class="w-9"
@@ -519,6 +519,54 @@
                                 />
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div>
+                    <div
+                        class="flex flex-col p-4 mb-10 text-center text-gray-300 bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700"
+                    >
+                        Request a Connection to this Server Using the Link: <span
+                            class="text-purple-400"
+                            >{window.location.hostname}</span
+                        >
+                    </div>
+                    <a
+                        href="https://microsocial.xyz"
+                        class="flex flex-col p-4 text-center bg-indigo-900 rounded-lg shadow-xl cursor-pointer group bg-opacity-40 ring-2 ring-offset-2 ring-offset-indigo-800 ring-indigo-700 hover:bg-indigo-800 dark:hover:bg-indigo-900 hover:bg-opacity-100 md:text-left md:flex-row md:items-center md:justify-between md:p-12"
+                    >
+                        <div class="text-2xl font-semibold">
+                            <div class="text-gray-300">
+                                Not Your Microsocial?
+                            </div>
+                            <div class="text-purple-500">
+                                Find out how to get your own in seconds
+                            </div>
+                        </div>
+
+                        <div class="mt-3 md:mt-0 md:ml-2">
+                            <div
+                                class="inline-block px-4 py-2 text-lg font-semibold text-white bg-purple-500 rounded-md"
+                            >
+                                Get Started
+                            </div>
+                        </div>
+                    </a>
+                    <div class="py-10 text-center text-gray-200">
+                        <a
+                            href="https://github.com/Gingerbreadfork/microsocial"
+                        >
+                            <svg
+                                role="img"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="inline w-5 h-5 mb-1 mr-2 text-center text-white fill-current"
+                            >
+                                <title>GitHub icon</title>
+                                <path
+                                    d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                                />
+                            </svg>Microsocial is Free & Open Source Software
+                        </a>
                     </div>
                 </div>
             </div>
