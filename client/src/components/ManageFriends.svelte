fd641823b921f17a543d8cd874245d0b2712794f
Gingerbreadfork <gingerbreadfork@protonmail.com>
2021-09-11 12:43:49 +1000

    Increased feature explanations

--- client/src/components/ManageFriends.svelte
+++ client/src/components/ManageFriends.svelte
@@ -281,21 +281,37 @@
                 >
             </div>
         </div>
-        {#if howDoYouConnect == true}
+        {#if howDoYouConnect == true || (actualFriendCount == 0 && pendingFriendCount == 0 && friendListLoaded == true)}
             <div
-                class="p-4 mt-4 mb-4 bg-gray-200 border-2 rounded-lg shadow-lg dark:bg-truegray-800 dark:border-truegray-900"
+                class="flex items-center justify-center p-8 my-6 bg-gray-200 shadow-md hover:shodow-lg rounded-2xl dark:bg-truegray-800"
             >
-                <p class="p-2 text-sm md:p-0">
-                    To connect with friends simply enter the URL that points to
-                    their Microsocial deployment in the form above, if they
-                    accept your request you'll get one back so don't forget to
-                    come back and accept it! Your Microsocial server URL is <span
-                        class="text-blue-800 dark:text-amber-400"
-                        >{hostBridge}</span
-                    > share it with your friends so they can connect with you. Friends
-                    and any pending requests will be displayed below where you can
-                    manage them and do things like view their profiles.
-                </p>
+                <div class="flex items-center">
+                    <svg
+                        class="w-20 h-20 text-purple-700 animate-pulse"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                        ><path
+                            fill-rule="evenodd"
+                            d="M9.504 1.132a1 1 0 01.992 0l1.75 1a1 1 0 11-.992 1.736L10 3.152l-1.254.716a1 1 0 11-.992-1.736l1.75-1zM5.618 4.504a1 1 0 01-.372 1.364L5.016 6l.23.132a1 1 0 11-.992 1.736L4 7.723V8a1 1 0 01-2 0V6a.996.996 0 01.52-.878l1.734-.99a1 1 0 011.364.372zm8.764 0a1 1 0 011.364-.372l1.733.99A1.002 1.002 0 0118 6v2a1 1 0 11-2 0v-.277l-.254.145a1 1 0 11-.992-1.736l.23-.132-.23-.132a1 1 0 01-.372-1.364zm-7 4a1 1 0 011.364-.372L10 8.848l1.254-.716a1 1 0 11.992 1.736L11 10.58V12a1 1 0 11-2 0v-1.42l-1.246-.712a1 1 0 01-.372-1.364zM3 11a1 1 0 011 1v1.42l1.246.712a1 1 0 11-.992 1.736l-1.75-1A1 1 0 012 14v-2a1 1 0 011-1zm14 0a1 1 0 011 1v2a1 1 0 01-.504.868l-1.75 1a1 1 0 11-.992-1.736L16 13.42V12a1 1 0 011-1zm-9.618 5.504a1 1 0 011.364-.372l.254.145V16a1 1 0 112 0v.277l.254-.145a1 1 0 11.992 1.736l-1.735.992a.995.995 0 01-1.022 0l-1.735-.992a1 1 0 01-.372-1.364z"
+                            clip-rule="evenodd"
+                        /></svg
+                    >
+                </div>
+                <div class="flex flex-col ml-3">
+                    <p class="p-2 text-sm md:p-0">
+                        To connect with friends simply enter the URL that points
+                        to their Microsocial deployment in the form above, if
+                        they accept your request you'll get one back so don't
+                        forget to come back and accept it! Your Microsocial
+                        server URL is <span
+                            class="text-blue-800 dark:text-amber-400"
+                            >{hostBridge}</span
+                        > share it with your friends so they can connect with you.
+                        Friends and any pending requests will be displayed below
+                        where you can manage them and do things like view their profiles.
+                    </p>
+                </div>
             </div>
         {/if}
 
--- client/src/components/Profile.svelte
+++ client/src/components/Profile.svelte
@@ -73,6 +73,36 @@
     {/if}
     {#if hostPostsLoaded}
         <h2 class="pt-4 pb-2 text-2xl">Posts</h2>
+        {#if hostPostsLoaded && hostPosts.length < 1}
+            <div
+                class="flex items-center justify-center p-8 my-6 bg-gray-200 shadow-md hover:shodow-lg rounded-2xl dark:bg-truegray-800"
+            >
+                <div class="flex items-center">
+                    <svg
+                        class="w-20 h-20 text-purple-700 animate-pulse"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                        ><path
+                            fill-rule="evenodd"
+                            d="M9.504 1.132a1 1 0 01.992 0l1.75 1a1 1 0 11-.992 1.736L10 3.152l-1.254.716a1 1 0 11-.992-1.736l1.75-1zM5.618 4.504a1 1 0 01-.372 1.364L5.016 6l.23.132a1 1 0 11-.992 1.736L4 7.723V8a1 1 0 01-2 0V6a.996.996 0 01.52-.878l1.734-.99a1 1 0 011.364.372zm8.764 0a1 1 0 011.364-.372l1.733.99A1.002 1.002 0 0118 6v2a1 1 0 11-2 0v-.277l-.254.145a1 1 0 11-.992-1.736l.23-.132-.23-.132a1 1 0 01-.372-1.364zm-7 4a1 1 0 011.364-.372L10 8.848l1.254-.716a1 1 0 11.992 1.736L11 10.58V12a1 1 0 11-2 0v-1.42l-1.246-.712a1 1 0 01-.372-1.364zM3 11a1 1 0 011 1v1.42l1.246.712a1 1 0 11-.992 1.736l-1.75-1A1 1 0 012 14v-2a1 1 0 011-1zm14 0a1 1 0 011 1v2a1 1 0 01-.504.868l-1.75 1a1 1 0 11-.992-1.736L16 13.42V12a1 1 0 011-1zm-9.618 5.504a1 1 0 011.364-.372l.254.145V16a1 1 0 112 0v.277l.254-.145a1 1 0 11.992 1.736l-1.735.992a.995.995 0 01-1.022 0l-1.735-.992a1 1 0 01-.372-1.364z"
+                            clip-rule="evenodd"
+                        /></svg
+                    >
+                </div>
+                <div class="flex flex-col ml-3">
+                    <div class="font-medium leading-none">
+                        You Don't Have Any Posts Just Yet!
+                    </div>
+                    <p
+                        class="mt-1 text-sm leading-none text-gray-600 dark:text-truegray-300"
+                    >
+                        Head over to the live feed tab and you can create some
+                        posts for your friends to see.
+                    </p>
+                </div>
+            </div>
+        {/if}
         {#each hostPosts as { value, time, reactions }}
             <div class="p-1">
                 <div
